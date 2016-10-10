51242c3a9f08aa69c5d750b0da5da4aa1fc6fbdf
Waleed Khan <me@waleedkhan.name>
2016-10-10 11:08:57 -0700

    Set up package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="stenodictate",
+    version="0.1",
+    description="Dictates text and grades transcriptions "
+                "for stenographer students.",
+    url="https://github.com/arxanas/StenoDictate",
+    author="Waleed Khan",
+    author_email="me@waleedkhan.name",
+    license="GPL3",
+    packages=["stenodictate"],
+)
