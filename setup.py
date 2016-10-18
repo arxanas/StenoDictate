d9f5a99efd023098b7beb47b5027ebd5ca406236
Waleed Khan <me@waleedkhan.name>
2016-10-18 16:45:42 -0700

    Mark pyobjc as required only on macOS

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+import re
+import sys
+
 from setuptools import setup
 
 try:
@@ -6,9 +9,41 @@ try:
 except ImportError:
     cmdclass = {}
 
+
+def _get_version():
+    with open("stenodictate/__init__.py") as f:
+        version = re.search(r"""
+            __version__
+            [ ]=[ ]
+            "
+                (?P<version>[^"]+)
+            "
+        """, f.read(), re.VERBOSE)
+        return version.group("version")
+
+
+def _get_requirements():
+    requirements = [
+        "appdirs>=1.4.0",
+        "py>=1.4.31",
+        "pyttsx>=1.1",
+        "textstat>=0.3.1",
+    ]
+
+    if "win32" in sys.platform:
+        # Should probably include PyWin32.
+        raise NotImplementedError()
+    elif "darwin" in sys.platform:
+        requirements.extend([
+            "pyobjc>=3.1.1",
+        ])
+
+    return requirements
+
+
 setup(
     name="stenodictate",
-    version="0.1",
+    version=_get_version(),
     description="Dictates text and grades transcriptions "
                 "for stenographer students.",
     url="https://github.com/arxanas/StenoDictate",
@@ -17,4 +52,5 @@ setup(
     license="GPL3",
     packages=["stenodictate"],
     cmdclass=cmdclass,
+    install_requires=_get_requirements(),
 )
